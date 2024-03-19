4dad9f4dd2f306d8df0e389310cafaa5a3cbe1e4
robin <techmaodao@gmail.com>
2024-03-19 23:52:45 +0800

    feat: update middleware

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "",
+  generateRobotsTxt: true, // (optional)
+  additionalPaths: async (config) => {
+    const result = [];
+
+    return result;
+  },
+};
