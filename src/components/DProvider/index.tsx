ae751dda59f38e74da4f7d856eada47cd11312c1
robin <liulixuan@snqu.com>
2024-04-09 16:31:15 +0800

    delete sharp

--- src/components/DProvider/index.tsx
+++ src/components/DProvider/index.tsx
@@ -43,7 +43,7 @@ export const DProvider = ({ children }: React.PropsWithChildren) => {
 
   useEffect(() => {
     init();
-  }, []);
+  }, [init]);
 
   return (
     <ThemeProvider theme={customeTheme}>
