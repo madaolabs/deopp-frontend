aeeacff49601bd301e119bb23373aaa7f2749811
robin <techmaodao@gmail.com>
2024-04-10 07:15:10 +0800

    feat: new sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,39 @@
+// next-sitemap.config.js
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "",
-  generateRobotsTxt: true, // (optional)
+  siteUrl: "https://deopp.dewall.xyz",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  // exclude: ["/server-sitemap.xml"], // <= exclude here
+  // robotsTxtOptions: {
+  //   additionalSitemaps: [
+  //     "https://news.madaotech.top/server-sitemap.xml", // <==== Add here
+  //   ],
+  // },
   additionalPaths: async (config) => {
-    const result = [];
+    const now = new Date().toISOString();
 
-    return result;
-  },
+    const lastmod = now.replace(/T.*/, '');
+    return [
+      {
+        loc: "https://deopp.dewall.xyz",
+        changefreq: "always",
+        priority: "1.0",
+        lastmod
+      },
+      {
+        loc: "https://deopp.dewall.xyz/en",
+        changefreq: "always",
+        priority: "0.9",
+        lastmod
+      },
+      {
+        loc: "https://deopp.dewall.xyz/zh",
+        changefreq: "always",
+        priority: "0.9",
+        lastmod
+      },
+    ]
+  }
 };
