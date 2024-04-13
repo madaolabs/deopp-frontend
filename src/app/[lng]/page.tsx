a19cebde6d70692e440fb176cd019b7107df7822
robin <techmaodao@gmail.com>
2024-04-13 10:43:34 +0800

    fix: server render

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,6 +1,29 @@
+import { getSalaryAverage } from "@/api";
 import { Home } from "@/client/Home";
+import { IPositionType } from "@/client/Home/types";
+import { usePublicStore } from "@/store/global";
 
 export default async function RootUrl({ params }: { params: { lng: string } }) {
-  // const Node = await Token({ params });
-  return <Home />;
+  let defaultPositionList: IPositionType[] = [];
+  let defaultAvgSalaryList = [];
+  try {
+    const { queryPositionList } = usePublicStore.getState();
+    await queryPositionList({ page: 1, pageSize: 70 });
+    const { positionList } = usePublicStore.getState();
+    defaultPositionList = positionList || [];
+    if (positionList && positionList.length) {
+      const serAvgSalary = await getSalaryAverage(positionList[0]?.id);
+      defaultAvgSalaryList = serAvgSalary?.companies || [];
+    }
+  } catch (error) {
+    console.log("server request error", error);
+  }
+  return (
+    <Home
+      defaultData={{
+        positionList: defaultPositionList,
+        avgSalaryList: defaultAvgSalaryList,
+      }}
+    />
+  );
 }
